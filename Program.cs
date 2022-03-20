37bb312d04dd5324a8975cfbf7f61a98d86c69a2
oguzhanncaan <oguzhancan.tr@outlook.com>
2022-03-20 16:26:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            //int uzunluk = 5;
+            //for(int i = 0; i <= uzunluk; i++)
+            //{
+            //    Console.Write(" ");
+            //    for (int j = i; j <= uzunluk; j++)
+            //    {
+            //        Console.Write(" * ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region WhileOrnek
+            //char harf = 'a';
+            //while (harf <= 'z')
+            //{
+            //    console.writeline(harf);
+            //    harf++;
+
+            //}
+            //console.readline();
+            #endregion
+            #region WhileOrnek2
+            //int sayi = 0;
+            //int toplam = 0;
+            //while (sayi <= 100)
+            //{
+            //    toplam += sayi;
+
+            //    sayi++;
+            //}
+            //Console.Write(toplam);
+            //Console.ReadLine();
+            #endregion
+            #region WhileOrnek3
+            //int number = 1;
+            //while (number != 0)
+            //{
+            //    Console.Write("Lütfen bir sayı giriniz:");
+            //    number = int.Parse(Console.ReadLine());
+            //} 
+            #endregion
+            #region WhileOrnek4
+            //int sayi = 0, toplam = 0;
+            //while (sayi % 2 == 0)
+            //{
+            //    toplam += sayi;
+            //    Console.WriteLine("Lütfen sayı giriniz:");
+            //    sayi = int.Parse(Console.ReadLine());
+
+
+            //}
+
+            //Console.Write("Toplam: " + toplam);
+            //Console.ReadLine(); 
+            #endregion
+
+
+        }
+
+    }
+}
